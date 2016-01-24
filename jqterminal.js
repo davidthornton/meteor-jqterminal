20452c6537f24e2e2072436c8444e75f88bfdbbf
David Thornton <david.m.thornton@gmail.com>
2016-01-24 21:00:33 +1100

    jQuery terminal wrapper package for Meteor is now ready for open source consumption

--- jqterminal.js
+++ jqterminal.js
@@ -1,12 +1,10 @@
-Template.jQTerminal.rendered = function() {
-    
-    var div = this.$(this.firstNode);
-    
-    // If values are undefined, it doesn't matter
-    div.attr('id',this.data.id);
-    div.addClass(this.data.class);
-    
-    // Initialize terminal() on the div
-    div.terminal(this.data.interpreter,this.data.options);
-
-}
+jQTerminal = (function() {
+    var instance = new Function();
+    _static = {
+        instance: function(id, interpreter, settings, callback){
+            var term = $(id).terminal(interpreter, settings);
+            callback(term);
+        }
+    }
+    return _static;
+})();
--- package.js
+++ package.js
@@ -2,26 +2,22 @@ Package.describe({
   name: 'meteor-jqterminal',
   version: '0.8.8',
   summary: 'Meteor package wrapper for jquery.terminal, a web browser-based terminal',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/davidthornton/meteor-jqterminal',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use([
-    'ecmascript',
-    'templating']);
+  api.use('ecmascript');
   
   api.addFiles([
     'lib/jquery.terminal-0.8.8.min.js',
     'lib/jquery.terminal.css'
   ],'client');
   
-  api.addFiles([
-    'jqterminal.html',
-    'jqterminal.js'
-    ],'client');
+  api.addFiles('jqterminal.js','client');
+
+  api.export('jQTerminal','client');
 
 });
 
